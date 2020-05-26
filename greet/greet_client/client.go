efeed701186f37a11dea9665e2ed6b9e3a3bc383
nakadayoshiki <y.nakada.rkt@gmail.com>
2020-05-26 23:00:48 +0900

    client done

---
+++ greet/greet_client/client.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/nakadayoshiki/grpc-go/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:500051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
